b1812e6d1f28d4881a839f0984978a55013109b3
onkar <onkarsawant111@gmail.com>
2024-07-05 21:25:37 +0530

    added register page using django.forms

--- register/views.py
+++ register/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import Register
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = Register(request.POST)
+        if form.is_valid():
+            form.save()
+            # user login after registration 
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have registered and logged in successfully!")
+            return redirect('home')
+        else:
+            # Print form errors to console for debugging
+            print(form.errors)
+            messages.error(request, "Oops! Something went wrong.")
+            return redirect('register')
+    else:
+        form = Register()
+    return render(request, 'register.html', {'form': form})
 
 def user_login(request):
     if request.method == "POST":
