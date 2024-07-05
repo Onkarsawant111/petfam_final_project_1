106621880c2311f858d6ecce7b62e421b80774c9
onkar <onkarsawant111@gmail.com>
2024-07-05 13:28:44 +0530

    added login form with authentication

--- register/views.py
+++ register/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 def register(request):
     return render(request, 'register.html')
 
-
-def login(request):
-   return render(request, 'login.html')
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("Username")
+        password = request.POST.get("Password")
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request, user) 
+            messages.success(request,("You have been logged in successfully!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error, please try again!")) 
+            return redirect('user_login')
+    else:
+        return render(request, 'login.html')
+    
