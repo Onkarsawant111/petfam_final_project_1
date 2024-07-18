5813cc75cb4d045df29808ae72e78ace55c587e3
onkar <onkarsawant111@gmail.com>
2024-07-18 20:54:12 +0530

    added checkout page and app so that user can see his complete order details

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Order, Order_items
+from cart.cart import Cart 
 
 # Create your views here.
 def checkout(request):
-    return render(request, 'checkout.html')
+    cart = Cart(request) 
+    total = cart.cart_total_price() # total price of products in cart
+    cart_products = cart.get_prods # call the function of 'cart.py' 
+    return render(request, 'checkout.html', {'cart_products':cart_products, 'total': total})
